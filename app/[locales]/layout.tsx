8fe2b53c51a672b9220ceef9d977a9b54f35875e
Djifack daril <darilvia64@gmail.com>
2026-08-24 21:30:43 +0100

    fix layout

--- app/[locales]/layout.tsx
+++ app/[locales]/layout.tsx
@@ -1,14 +1,18 @@
 import { notFound } from "next/navigation";
+import { setStaticParamsLocale } from 'next-international/server';
 import { I18nProviderClient } from "@/locales/client";
+import { getStaticParams } from '.@/locales/server'
 
 const locales = ["fr", "en"] as const;
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locales: locale }));
+  return getStaticParams();
 }
 
 export default async function LocaleLayout({ children, params }: { children: React.ReactNode; params: Promise<{ locales: string }> }) {
   const { locales: locale } = await params;
+  setStaticParamsLocale(locale);
 
   if (!locales.includes(locale as (typeof locales)[number])) {
     notFound();
